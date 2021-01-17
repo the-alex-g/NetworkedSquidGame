ac8db360b2d5f324f1bda1a03d8b261903b58c3d
Alex Gestwicki <alex.gestwicki@gmail.com>
2021-01-17 14:01:12 -0500

    Add winner stuff

--- project/Game/Bullets/Bullet.gd
+++ project/Game/Bullets/Bullet.gd
@@ -11,6 +11,8 @@ func _ready():
 
 func _process(delta):
 	position += Vector2(0,speed*delta).rotated(rotation)
+	if get_tree().paused:
+		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
--- project/Game/Game.gd
+++ project/Game/Game.gd
@@ -3,8 +3,13 @@ extends Node2D
 const _Player := preload("res://Game/Player/Player.tscn")
 
 onready var _players := $Players
+onready var squid := $SquidBody/ColoredSquidParts
+
+var max_score:int
 
 func _ready():
+	$AnimationPlayer2.play("Wave_Arms")
+	max_score = Global.colors.size()*5
 	var _rotation := 0.0
 	var _rotation_increment := 360.0/Global.player_colors.size()
 	for player_id in Global.player_colors:
@@ -16,3 +21,15 @@ func _ready():
 		player.position = Vector2(350,350)
 		_rotation += _rotation_increment
 		_players.add_child(player)
+
+func _process(delta):
+	var scores := Global.score
+	for score in scores:
+		if scores[score] >= max_score:
+			_get_winner(score)
+
+func _get_winner(color):
+	_players.hide()
+	squid.modulate = color
+	$AnimationPlayer.play("Winner")
+	get_tree().paused = true
