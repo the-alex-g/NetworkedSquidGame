064808c078e6fafaff6b4602a1c90120839d9db8
Alex Gestwicki <alex.gestwicki@gmail.com>
2021-01-17 14:06:04 -0500

    Add squids wave arms during wait time

--- project/PlayerIntegration/WaitingArea/PlayerIndicator.gd
+++ project/PlayerIntegration/WaitingArea/PlayerIndicator.gd
@@ -6,6 +6,9 @@ func _ready():
 	var secs = rand_range(0,6)
 	$AnimationPlayer.play("Fade")
 	$AnimationPlayer.seek(secs, true)
+	secs = rand_range(0,8)
+	$WaveArms.play("Wave_Arms")
+	$WaveArms.seek(secs, true)
 
 func _process(_delta):
 	if color != null:
